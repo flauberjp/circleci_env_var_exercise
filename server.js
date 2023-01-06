114e309c93a847fb0a64aca4167e1c0772f06b78
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2023-01-06 09:54:54 -0300

    add printFavoriteFood func

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function printFavoriteFood() {
+  const favoriteFood = process.env.FAVORITE_FOOD;
+  console.log(`My favorite food is ${favoriteFood}`);
 }
 
 async function main() {
-  while(true) {
+  while (true) {
     console.log("Microservices rock!");
+    printFavoriteFood();
     await sleep(5000);
   }
 }
